8a2d39ca6d607dd5c825ce4f42585be8216e56f8
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-07-29 18:21:36 +0200

    Filtered publication

--- gradle-conventions/src/main/kotlin/conventions-publishing.gradle.kts
+++ gradle-conventions/src/main/kotlin/conventions-publishing.gradle.kts
@@ -38,6 +38,8 @@ fun PublishingExtension.configurePublication() {
         null
     }
 
+    val remove = mutableSetOf<MavenPublication>()
+
     // separate function is needed for different gradle versions
     // in 7.6 `Configuration` argument is `this`, in 8.* it is a first argument (hence `it`)
     val onPublication: (MavenPublication) -> Unit = { publication ->
@@ -49,6 +51,19 @@ fun PublishingExtension.configurePublication() {
 
         publication.setPublicArtifactId(project)
 
+        val suffix = when (publication.name) {
+            "kotlinMultiplatform" -> ""
+            else -> "-${publication.name.lowercase()}"
+        }
+
+        print("test publication: ${publication.artifactId + suffix} -> ")
+        if (publication.artifactId + suffix !in missing) {
+            remove.add(publication)
+            println("REJECT")
+        } else {
+            println("OK")
+        }
+
         if (!isGradlePlugin) {
             publication.fixModuleMetadata(project)
         }
@@ -57,6 +72,7 @@ fun PublishingExtension.configurePublication() {
     }
 
     publications.withType(MavenPublication::class).all(onPublication)
+    publications.removeAll(remove)
 
     tasks.withType<PublishToMavenRepository>().configureEach {
         dependsOn(tasks.withType<Sign>())
@@ -168,3 +184,119 @@ fun MavenPublication.signPublicationIfKeyPresent() {
         }
     }
 }
+
+val missing = setOf(
+    "kotlinx-rpc-krpc-logging-linuxx64",
+    "kotlinx-rpc-krpc-logging-macosarm64",
+    "kotlinx-rpc-krpc-logging-macosx64",
+    "kotlinx-rpc-krpc-logging-tvosarm64",
+    "kotlinx-rpc-krpc-logging-tvosx64",
+    "kotlinx-rpc-krpc-logging-watchosarm64",
+    "kotlinx-rpc-krpc-logging-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-logging-watchosx64",
+    "kotlinx-rpc-krpc-serialization-cbor",
+    "kotlinx-rpc-krpc-serialization-cbor-iosarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-iossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-iosx64",
+    "kotlinx-rpc-krpc-serialization-cbor-js",
+    "kotlinx-rpc-krpc-serialization-cbor-jvm",
+    "kotlinx-rpc-krpc-serialization-cbor-linuxarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-linuxx64",
+    "kotlinx-rpc-krpc-serialization-cbor-macosarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-macosx64",
+    "kotlinx-rpc-krpc-serialization-cbor-tvosarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-tvosx64",
+    "kotlinx-rpc-krpc-serialization-cbor-watchosarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-cbor-watchosx64",
+    "kotlinx-rpc-krpc-serialization-core",
+    "kotlinx-rpc-krpc-serialization-core-iosarm64",
+    "kotlinx-rpc-krpc-serialization-core-iossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-core-iosx64",
+    "kotlinx-rpc-krpc-serialization-core-js",
+    "kotlinx-rpc-krpc-serialization-core-jvm",
+    "kotlinx-rpc-krpc-serialization-core-linuxarm64",
+    "kotlinx-rpc-krpc-serialization-core-linuxx64",
+    "kotlinx-rpc-krpc-serialization-core-macosarm64",
+    "kotlinx-rpc-krpc-serialization-core-macosx64",
+    "kotlinx-rpc-krpc-serialization-core-tvosarm64",
+    "kotlinx-rpc-krpc-serialization-core-tvosx64",
+    "kotlinx-rpc-krpc-serialization-core-watchosarm64",
+    "kotlinx-rpc-krpc-serialization-core-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-core-watchosx64",
+    "kotlinx-rpc-krpc-serialization-json",
+    "kotlinx-rpc-krpc-serialization-json-iosarm64",
+    "kotlinx-rpc-krpc-serialization-json-iossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-json-iosx64",
+    "kotlinx-rpc-krpc-serialization-json-js",
+    "kotlinx-rpc-krpc-serialization-json-jvm",
+    "kotlinx-rpc-krpc-serialization-json-linuxarm64",
+    "kotlinx-rpc-krpc-serialization-json-linuxx64",
+    "kotlinx-rpc-krpc-serialization-json-macosarm64",
+    "kotlinx-rpc-krpc-serialization-json-macosx64",
+    "kotlinx-rpc-krpc-serialization-json-tvosarm64",
+    "kotlinx-rpc-krpc-serialization-json-tvosx64",
+    "kotlinx-rpc-krpc-serialization-json-watchosarm64",
+    "kotlinx-rpc-krpc-serialization-json-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-json-watchosx64",
+    "kotlinx-rpc-krpc-serialization-protobuf",
+    "kotlinx-rpc-krpc-serialization-protobuf-iosarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-iossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-iosx64",
+    "kotlinx-rpc-krpc-serialization-protobuf-js",
+    "kotlinx-rpc-krpc-serialization-protobuf-jvm",
+    "kotlinx-rpc-krpc-serialization-protobuf-linuxarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-linuxx64",
+    "kotlinx-rpc-krpc-serialization-protobuf-macosarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-macosx64",
+    "kotlinx-rpc-krpc-serialization-protobuf-tvosarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-tvosx64",
+    "kotlinx-rpc-krpc-serialization-protobuf-watchosarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-serialization-protobuf-watchosx64",
+    "kotlinx-rpc-krpc-server",
+    "kotlinx-rpc-krpc-server-iosarm64",
+    "kotlinx-rpc-krpc-server-iossimulatorarm64",
+    "kotlinx-rpc-krpc-server-iosx64",
+    "kotlinx-rpc-krpc-server-js",
+    "kotlinx-rpc-krpc-server-jvm",
+    "kotlinx-rpc-krpc-server-linuxarm64",
+    "kotlinx-rpc-krpc-server-linuxx64",
+    "kotlinx-rpc-krpc-server-macosarm64",
+    "kotlinx-rpc-krpc-server-macosx64",
+    "kotlinx-rpc-krpc-server-tvosarm64",
+    "kotlinx-rpc-krpc-server-tvosx64",
+    "kotlinx-rpc-krpc-server-watchosarm64",
+    "kotlinx-rpc-krpc-server-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-server-watchosx64",
+    "kotlinx-rpc-krpc-test",
+    "kotlinx-rpc-krpc-test-iosarm64",
+    "kotlinx-rpc-krpc-test-iossimulatorarm64",
+    "kotlinx-rpc-krpc-test-iosx64",
+    "kotlinx-rpc-krpc-test-js",
+    "kotlinx-rpc-krpc-test-jvm",
+    "kotlinx-rpc-krpc-test-linuxarm64",
+    "kotlinx-rpc-krpc-test-linuxx64",
+    "kotlinx-rpc-krpc-test-macosarm64",
+    "kotlinx-rpc-krpc-test-macosx64",
+    "kotlinx-rpc-krpc-test-tvosarm64",
+    "kotlinx-rpc-krpc-test-tvosx64",
+    "kotlinx-rpc-krpc-test-watchosarm64",
+    "kotlinx-rpc-krpc-test-watchossimulatorarm64",
+    "kotlinx-rpc-krpc-test-watchosx64",
+    "kotlinx-rpc-utils",
+    "kotlinx-rpc-utils-iosarm64",
+    "kotlinx-rpc-utils-iossimulatorarm64",
+    "kotlinx-rpc-utils-iosx64",
+    "kotlinx-rpc-utils-js",
+    "kotlinx-rpc-utils-jvm",
+    "kotlinx-rpc-utils-linuxarm64",
+    "kotlinx-rpc-utils-linuxx64",
+    "kotlinx-rpc-utils-macosarm64",
+    "kotlinx-rpc-utils-macosx64",
+    "kotlinx-rpc-utils-tvosarm64",
+    "kotlinx-rpc-utils-tvosx64",
+    "kotlinx-rpc-utils-watchosarm64",
+    "kotlinx-rpc-utils-watchossimulatorarm64",
+    "kotlinx-rpc-utils-watchosx64"
+)
