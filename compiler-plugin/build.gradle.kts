f869cf9562386d935824ed44286c722fd9a3dbe0
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-08-13 20:05:19 +0200

    Removed meta tasks. Gradle -p can be used instead

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -2,8 +2,6 @@
  * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-import util.configureMetaTasks
-
 val kotlinVersion: KotlinVersion by extra
 val rpcVersion: String = libs.versions.kotlinx.rpc.get()
 
@@ -11,6 +9,3 @@ allprojects {
     group = "org.jetbrains.kotlinx"
     version = "$kotlinVersion-$rpcVersion"
 }
-
-configureMetaTasks("clean", "cleanTest", "test", "build")
-configureMetaTasks(tasks.matching { it.name.startsWith("publish") }.map { it.name })
