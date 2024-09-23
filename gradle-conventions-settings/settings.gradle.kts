1be14880cd79a4ec631c9ead55629315585de88f
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2024-09-23 19:47:36 +0200

    Added build cache and develocity plugin (#192)

--- gradle-conventions-settings/settings.gradle.kts
+++ gradle-conventions-settings/settings.gradle.kts
@@ -6,3 +6,5 @@ rootProject.name = "gradle-conventions-settings"
 
 // Code below is a hack because a chicken-egg problem, I can't use myself as a settings-plugin
 apply(from = "src/main/kotlin/settings-conventions.settings.gradle.kts")
+
+include(":develocity")
