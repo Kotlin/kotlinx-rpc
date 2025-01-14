f634c1e88cf09bca67d7f549346c1a0b9e8bd1b7
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-01-14 16:38:48 +0100

    Added missing aliases for migration (#254)

--- core/src/commonMain/kotlin/kotlinx/rpc/RpcCall.kt
+++ core/src/commonMain/kotlin/kotlinx/rpc/RpcCall.kt
@@ -1,10 +1,13 @@
 /*
- * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package kotlinx.rpc
 
 import kotlinx.rpc.descriptor.RpcServiceDescriptor
+
+@Deprecated("Use RpcCall instead", ReplaceWith("RpcCall"), level = DeprecationLevel.ERROR)
+public typealias RPCCall = RpcCall
 
 /**
  * Represents a method or field call of an RPC service.
