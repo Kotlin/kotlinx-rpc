f100dd01669b7cf8a823970ca4e764fdece99f82
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-01-17 16:01:58 +0100

    gRPC sample app

---
+++ samples/grpc-app/src/main/kotlin/Client.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.runBlocking
+import kotlinx.rpc.grpc.GrpcClient
+import kotlinx.rpc.withService
+
+fun main(): Unit = runBlocking {
+    val grpcClient = GrpcClient("localhost", 8080) {
+        usePlaintext()
+    }
+
+    val recognizer = grpcClient.withService<ImageRecognizer>()
+
+    val image = Image {
+        data = byteArrayOf(0, 1, 2, 3)
+    }
+    val result = recognizer.recognize(image)
+    println("Recognized category: ${result.category}")
+
+    grpcClient.cancel()
+}
