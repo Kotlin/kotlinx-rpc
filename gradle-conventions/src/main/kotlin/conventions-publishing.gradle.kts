9273d6597ac9476977f624ee78c5d73bc206c6f6
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-01-29 20:08:05 +0100

    Update for IDE configs

--- gradle-conventions/src/main/kotlin/conventions-publishing.gradle.kts
+++ gradle-conventions/src/main/kotlin/conventions-publishing.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 import util.*
@@ -53,7 +53,7 @@ fun PublishingExtension.configurePublication() {
             fixModuleMetadata(project)
         }
 
-        logger.info("Project ${project.name} -> Publication configured: $name")
+        logger.info("Project ${project.name} -> Publication configured: $name, $version")
     }
 
     tasks.withType<PublishToMavenRepository>().configureEach {
