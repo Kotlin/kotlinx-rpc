0e8255de45e4f91fd6f72f12a537d979866a503a
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-02-11 14:40:16 +0100

    Update version to 0.5.1

--- samples/simple-ktor-app/build.gradle.kts
+++ samples/simple-ktor-app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("jvm") version "2.1.10"
     kotlin("plugin.serialization") version "2.1.10"
     id("io.ktor.plugin") version "3.0.3"
-    id("org.jetbrains.kotlinx.rpc.plugin") version "0.5.0"
+    id("org.jetbrains.kotlinx.rpc.plugin") version "0.5.1"
 }
 
 group = "kotlinx.rpc.sample"
@@ -28,12 +28,12 @@ kotlin {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-client:0.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-server:0.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-serialization-json:0.5.1")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.5.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.5.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-client:0.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-rpc-krpc-ktor-server:0.5.1")
 
     implementation("io.ktor:ktor-client-cio")
     implementation("io.ktor:ktor-server-netty-jvm")
