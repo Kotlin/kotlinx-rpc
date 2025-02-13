d5401ea41675d833444039096bdd8383eadd1de3
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-02-13 13:54:29 +0100

    Simplified grpc gradle config (#281)
    * Simplified grpc gradle config
    
    * Enabled explicit API for the Gradle Plugin

--- gradle-plugin/src/main/kotlin/kotlinx/rpc/compilerPlugins.kt
+++ gradle-plugin/src/main/kotlin/kotlinx/rpc/compilerPlugins.kt
@@ -10,19 +10,19 @@ import org.gradle.kotlin.dsl.findByType
 import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
 import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
 
-class CompilerPluginK2 : KotlinCompilerPluginSupportPlugin by compilerPlugin({
+internal class CompilerPluginK2 : KotlinCompilerPluginSupportPlugin by compilerPlugin({
     pluginSuffix = "-k2"
 })
 
-class CompilerPluginCommon : KotlinCompilerPluginSupportPlugin by compilerPlugin({
+internal class CompilerPluginCommon : KotlinCompilerPluginSupportPlugin by compilerPlugin({
     pluginSuffix = "-common"
 })
 
-class CompilerPluginBackend : KotlinCompilerPluginSupportPlugin by compilerPlugin({
+internal class CompilerPluginBackend : KotlinCompilerPluginSupportPlugin by compilerPlugin({
     pluginSuffix = "-backend"
 })
 
-class CompilerPluginCli : KotlinCompilerPluginSupportPlugin by compilerPlugin({
+internal class CompilerPluginCli : KotlinCompilerPluginSupportPlugin by compilerPlugin({
     pluginSuffix = "-cli"
 
     applyToCompilation = {
