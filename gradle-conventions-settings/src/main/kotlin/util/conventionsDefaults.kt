0b5d33408829745b0bbccb0aa12bb25307740051
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-02-14 15:03:05 +0100

    Fix build config for for-ide builds (#287)

--- gradle-conventions-settings/src/main/kotlin/util/conventionsDefaults.kt
+++ gradle-conventions-settings/src/main/kotlin/util/conventionsDefaults.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package util
@@ -9,7 +9,6 @@ import org.gradle.api.Project
 fun Project.defaultConventionConfiguration() {
     configurations.configureEach {
         resolutionStrategy {
-            force(libs.kotlin.reflect)
             force(libs.kotlin.stdlib)
             force(libs.kotlin.stdlib.jdk7)
             force(libs.kotlin.stdlib.jdk8)
