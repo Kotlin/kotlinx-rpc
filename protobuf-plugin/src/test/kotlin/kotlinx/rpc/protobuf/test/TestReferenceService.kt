54f5f69ede4dfbc3bf5cbed6176cb6405b1f4b40
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-02-25 09:47:26 +0100

    Enum support (#294)

--- protobuf-plugin/src/test/kotlin/kotlinx/rpc/protobuf/test/TestReferenceService.kt
+++ protobuf-plugin/src/test/kotlin/kotlinx/rpc/protobuf/test/TestReferenceService.kt
@@ -26,20 +26,16 @@ class ReferenceTestServiceImpl : ReferenceTestService {
             primitive = message.other.arg
         }
     }
+
+    override suspend fun Enum(message: UsingEnum): UsingEnum {
+        return message
+    }
 }
 
 class TestReferenceService {
     @Test
     fun testReferenceService()= runBlocking {
-        val grpcClient = GrpcClient("localhost", 8080) {
-            usePlaintext()
-        }
-
-        val grpcServer = GrpcServer(8080) {
-            registerService<ReferenceTestService> { ReferenceTestServiceImpl() }
-        }
-
-        grpcServer.start()
+        val grpcClient = initializeServerAndClient()
 
         val service = grpcClient.withService<ReferenceTestService>()
         val result = service.Get(References {
@@ -50,5 +46,30 @@ class TestReferenceService {
 
         assertEquals("42", result.primitive)
         assertEquals(42, result.other.field)
+    }
+
+    @Test
+    fun testEnum() = runBlocking {
+        val grpcClient = initializeServerAndClient()
+
+        val service = grpcClient.withService<ReferenceTestService>()
+        val result = service.Enum(UsingEnum {
+            enum = Enum.ONE
+        })
+
+        assertEquals(Enum.ONE, result.enum)
+    }
+
+    private fun initializeServerAndClient(): GrpcClient {
+        val grpcClient = GrpcClient("localhost", 8080) {
+            usePlaintext()
+        }
+
+        val grpcServer = GrpcServer(8080) {
+            registerService<ReferenceTestService> { ReferenceTestServiceImpl() }
+        }
+
+        grpcServer.start()
+        return grpcClient
     }
 }
