9f758114de20d7119b6ee3013ca42b3042467283
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-03-11 22:38:02 +0100

    uncomment subplugin options

--- gradle-plugin/src/main/kotlin/kotlinx/rpc/compilerPlugins.kt
+++ gradle-plugin/src/main/kotlin/kotlinx/rpc/compilerPlugins.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 @file:Suppress("detekt.ClassNaming", "ClassName")
@@ -36,12 +36,11 @@ class CompilerPluginCli : KotlinCompilerPluginSupportPlugin by compilerPlugin({
                 SubpluginOption("strict-stateFlow", strict.stateFlow.get().toCompilerArg()),
                 SubpluginOption("strict-sharedFlow", strict.sharedFlow.get().toCompilerArg()),
                 SubpluginOption("strict-nested-flow", strict.nestedFlow.get().toCompilerArg()),
-                // WIP: https://youtrack.jetbrains.com/issue/KRPC-133
-//                SubpluginOption("strict-stream-scope", strict.streamScopedFunctions.get().toCompilerArg()),
-//                SubpluginOption(
-//                    "strict-suspending-server-streaming",
-//                    strict.suspendingServerStreaming.get().toCompilerArg()
-//                ),
+                SubpluginOption("strict-stream-scope", strict.streamScopedFunctions.get().toCompilerArg()),
+                SubpluginOption(
+                    "strict-suspending-server-streaming",
+                    strict.suspendingServerStreaming.get().toCompilerArg()
+                ),
                 SubpluginOption("strict-not-top-level-server-flow", strict.notTopLevelServerFlow.get().toCompilerArg()),
                 SubpluginOption("strict-fields", strict.fields.get().toCompilerArg()),
                 @OptIn(RpcDangerousApi::class)
