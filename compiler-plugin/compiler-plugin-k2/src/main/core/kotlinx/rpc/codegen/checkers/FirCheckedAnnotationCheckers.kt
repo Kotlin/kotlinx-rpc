a7e722d589d4ebda9ed0b16de5c3cd1e620ceac2
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-03-17 12:41:17 +0100

    Confusing compilation error when not specifying type argument to registerService #288

--- compiler-plugin/compiler-plugin-k2/src/main/core/kotlinx/rpc/codegen/checkers/FirCheckedAnnotationCheckers.kt
+++ compiler-plugin/compiler-plugin-k2/src/main/core/kotlinx/rpc/codegen/checkers/FirCheckedAnnotationCheckers.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
 package kotlinx.rpc.codegen.checkers
@@ -231,6 +231,7 @@ object FirCheckedAnnotationHelper {
                 source = source,
                 context = context,
                 reporter = reporter,
+                typeArgumentIndex = i,
             )
         }
 
@@ -304,6 +305,7 @@ object FirCheckedAnnotationHelper {
         source: KtSourceElement?,
         context: CheckerContext,
         reporter: DiagnosticReporter,
+        typeArgumentIndex: Int,
     ) {
         for (annotationClass in annotations) {
             val hasCheckedAnnotation = hasCheckedAnnotation(
@@ -316,7 +318,9 @@ object FirCheckedAnnotationHelper {
                 reporter.reportOn(
                     source = source,
                     factory = FirRpcDiagnostics.CHECKED_ANNOTATION_VIOLATION,
-                    a = annotationClass.defaultType(),
+                    a = typeArgumentIndex,
+                    b = annotationClass.defaultType(),
+                    c = classSymbol,
                     context = context,
                 )
             }
