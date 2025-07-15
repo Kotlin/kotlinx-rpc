e1ac247f8c09580cba8cecbe6c9fd80355a5c692
Johannes Zottele <official@johannes-zottele.at>
2025-07-15 19:42:05 +0200

    grpc-native: Intial setup with minimal C++/C/Kotlin interface (#400)
    * grpc-native: Add grpcpp-c C/C++ library to grpc/
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add gradle configuration for cInterop and grpcpp_c library
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add basic C Kotlin wrapper classes
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Update platforms.topic
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Resolve review requests
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

---
+++ grpc/grpc-core/src/nativeTest/kotlin/kotlinx/rpc/grpc/BridgeTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc
+
+import kotlinx.coroutines.runBlocking
+import kotlinx.rpc.grpc.bridge.GrpcByteBuffer
+import kotlinx.rpc.grpc.bridge.GrpcClient
+import kotlinx.rpc.grpc.bridge.GrpcSlice
+import libgrpcpp_c.pb_decode_greeter_sayhello_response
+import kotlin.test.Test
+
+@OptIn(kotlinx.cinterop.ExperimentalForeignApi::class)
+class BridgeTest {
+
+    @Test
+    fun testBasicUnaryAsyncCall() {
+        runBlocking {
+            GrpcClient("localhost:50051").use { client ->
+                GrpcSlice(byteArrayOf(8, 4)).use { request ->
+                    GrpcByteBuffer(request).use { req_buf ->
+                        client.callUnary("/Greeter/SayHello", req_buf)
+                            .use { result ->
+                                result.intoSlice().use { response ->
+                                    val value = pb_decode_greeter_sayhello_response(response.cSlice)
+                                    println("Response received: $value")
+                                }
+
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
