d5ac43353c17a8ba77f1e5c6df576732cec863b5
Johannes Zottele <official@johannes-zottele.at>
2025-07-15 19:42:05 +0200

    grpc-native: Intial setup with minimal C++/C/Kotlin interface (#400)
    * grpc-native: Add grpcpp-c C/C++ library to grpc/
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add gradle configuration for cInterop and grpcpp_c library
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add basic C Kotlin wrapper classes
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Update platforms.topic
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Resolve review requests
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/build.gradle.kts
+++ grpc/grpc-core/build.gradle.kts
@@ -5,10 +5,9 @@
 import kotlinx.rpc.buf.tasks.BufGenerateTask
 import kotlinx.rpc.proto.kxrpc
 import org.gradle.kotlin.dsl.withType
-
-/*
- * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+import org.gradle.internal.extensions.stdlib.capitalized
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
 
 plugins {
     alias(libs.plugins.conventions.kmp)
@@ -58,9 +57,61 @@ kotlin {
                 implementation(libs.protobuf.kotlin)
             }
         }
+
+        nativeTest {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+    }
+
+    val grpcppCLib = projectDir.resolve("../grpcpp-c")
+
+    // TODO: Replace function implementation, so it does not use an internal API
+    fun findProgram(name: String) = org.gradle.internal.os.OperatingSystem.current().findInPath(name)
+    val checkBazel by tasks.registering {
+        doLast {
+            val bazelPath = findProgram("bazel")
+            if (bazelPath != null) {
+                logger.debug("bazel: {}", bazelPath)
+            } else {
+                throw GradleException("'bazel' not found on PATH. Please install Bazel (https://bazel.build/).")
+            }
+        }
+    }
+
+    val buildGrpcppCLib = tasks.register<Exec>("buildGrpcppCLib") {
+        group = "build"
+        workingDir = grpcppCLib
+        commandLine("bash", "-c", "bazel build :grpcpp_c_static --config=release")
+        inputs.files(fileTree(grpcppCLib) { exclude("bazel-*/**") })
+        outputs.dir(grpcppCLib.resolve("bazel-bin"))
+
+        dependsOn(checkBazel)
+    }
+
+
+    targets.filterIsInstance<KotlinNativeTarget>().forEach {
+        it.compilations.getByName("main") {
+            cinterops {
+                val libgrpcpp_c by creating {
+                    includeDirs(
+                        grpcppCLib.resolve("include"),
+                        grpcppCLib.resolve("bazel-grpcpp-c/external/grpc+/include")
+                    )
+                    extraOpts(
+                        "-libraryPath", "${grpcppCLib.resolve("bazel-out/darwin_arm64-opt/bin")}",
+                    )
+                }
+
+                val interopTask = "cinterop${libgrpcpp_c.name.capitalized()}${it.targetName.capitalized()}"
+                tasks.named(interopTask, CInteropProcess::class) {
+                    dependsOn(buildGrpcppCLib)
+                }
+            }
+        }
     }
 }
-
 
 protoSourceSets {
     jvmTest {
