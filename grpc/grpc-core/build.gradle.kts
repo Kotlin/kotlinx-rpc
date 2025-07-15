16c76c330ad57dfd4cec16447da3be9722f33c9a
Johannes Zottele <official@johannes-zottele.at>
2025-07-15 16:06:12 +0200

    grpc-native: Add gradle configuration for cInterop and grpcpp_c library
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/build.gradle.kts
+++ grpc/grpc-core/build.gradle.kts
@@ -2,6 +2,9 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import org.gradle.internal.extensions.stdlib.capitalized
+import util.createCInterop
+
 plugins {
     alias(libs.plugins.conventions.kmp)
     alias(libs.plugins.kotlinx.rpc)
@@ -28,4 +31,45 @@ kotlin {
             }
         }
     }
+
+    val grpcppCLib = projectDir.resolve("../grpcpp-c")
+
+    fun findProgram(name: String) = org.gradle.internal.os.OperatingSystem.current().findInPath(name)
+    val checkBazel by tasks.registering {
+        doLast {
+            val bazelPath = findProgram("bazel")
+            if (bazelPath != null) {
+                println("bazel: $bazelPath")
+            } else {
+                throw GradleException("'bazel' not found on PATH. Please install Bazel (https://bazel.build/).")
+            }
+        }
+    }
+
+    val buildGrpcppCLib = tasks.register<Exec>("buildGrpcppCLib") {
+        group = "build"
+        workingDir = grpcppCLib
+        commandLine("bash", "-c", "bazel build :grpcpp_c_static --config=release")
+        inputs.files(fileTree(grpcppCLib) { exclude("bazel-*/**") })
+        outputs.dir(grpcppCLib.resolve("bazel-bin"))
+
+        dependsOn(checkBazel)
+    }
+
+    createCInterop("libgrpcpp_c", sourceSet = "src/nativeMain") { target ->
+        includeDirs(grpcppCLib.resolve("include"))
+        extraOpts(
+            "-libraryPath", "${grpcppCLib.resolve("bazel-out/darwin_arm64-opt/bin")}"
+        )
+        includeDirs(
+            grpcppCLib.resolve("include"),
+            grpcppCLib.resolve("bazel-grpcpp-c/external/grpc+/include")
+        )
+
+        tasks.named("cinteropLibgrpcpp_c${target.capitalized()}") {
+            dependsOn(buildGrpcppCLib)
+        }
+
+    }
+
 }
