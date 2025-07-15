28807578875af86a07a30d466acf8d11e2739bda
Johannes Zottele <official@johannes-zottele.at>
2025-07-15 19:42:05 +0200

    grpc-native: Intial setup with minimal C++/C/Kotlin interface (#400)
    * grpc-native: Add grpcpp-c C/C++ library to grpc/
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add gradle configuration for cInterop and grpcpp_c library
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Add basic C Kotlin wrapper classes
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Update platforms.topic
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Resolve review requests
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

---
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/bridge/GrpcByteBuffer.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc.bridge
+
+import kotlinx.cinterop.*
+import libgrpcpp_c.*
+
+@OptIn(ExperimentalForeignApi::class)
+internal class GrpcByteBuffer internal constructor(
+    internal val cByteBuffer: CPointer<grpc_byte_buffer>
+) : AutoCloseable {
+
+    constructor(slice: GrpcSlice) : this(memScoped {
+        grpc_raw_byte_buffer_create(slice.cSlice, 1u) ?: error("Failed to create byte buffer")
+    })
+
+    fun intoSlice(): GrpcSlice {
+        memScoped {
+            val respSlice = alloc<grpc_slice>()
+            grpc_byte_buffer_dump_to_single_slice(cByteBuffer, respSlice.ptr)
+            return GrpcSlice(respSlice.readValue())
+        }
+    }
+
+    override fun close() {
+        grpc_byte_buffer_destroy(cByteBuffer)
+    }
+
+}
---
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/bridge/GrpcSlice.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc.bridge
+
+import kotlinx.cinterop.CValue
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.usePinned
+import libgrpcpp_c.grpc_slice
+import libgrpcpp_c.grpc_slice_from_copied_buffer
+import libgrpcpp_c.grpc_slice_unref
+
+@OptIn(ExperimentalForeignApi::class)
+internal class GrpcSlice internal constructor(internal val cSlice: CValue<grpc_slice>) : AutoCloseable {
+
+    constructor(buffer: ByteArray) : this(
+        buffer.usePinned { pinned ->
+            grpc_slice_from_copied_buffer(pinned.addressOf(0), buffer.size.toULong())
+        }
+    )
+
+    override fun close() {
+        grpc_slice_unref(cSlice)
+    }
+}
