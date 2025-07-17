2a4ad40fc396960f848fbf2d9d9bdff626f3a18a
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-07-17 12:04:43 +0200

    gRPC: Ktor Plugin (#393)

--- grpc/grpc-core/src/jvmMain/kotlin/kotlinx/rpc/grpc/ManagedChannel.jvm.kt
+++ grpc/grpc-core/src/jvmMain/kotlin/kotlinx/rpc/grpc/ManagedChannel.jvm.kt
@@ -24,8 +24,8 @@ internal actual fun ManagedChannelBuilder<*>.buildChannel(): ManagedChannel {
     return build().toKotlin()
 }
 
-internal actual fun ManagedChannelBuilder(name: String, port: Int): ManagedChannelBuilder<*> {
-    return io.grpc.ManagedChannelBuilder.forAddress(name, port)
+internal actual fun ManagedChannelBuilder(hostname: String, port: Int): ManagedChannelBuilder<*> {
+    return io.grpc.ManagedChannelBuilder.forAddress(hostname, port)
 }
 
 internal actual fun ManagedChannelBuilder(target: String): ManagedChannelBuilder<*> {
