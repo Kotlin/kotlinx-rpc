03ab1554cf8f79a1bfb7860b7ba9a21aa4367779
Johannes Zottele <official@johannes-zottele.at>
2025-07-18 17:17:40 +0200

    grpc-native: Add primitive value encoding/decoding
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcByteBuffer.kt
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcByteBuffer.kt
@@ -6,15 +6,23 @@ package kotlinx.rpc.grpc.internal.bridge
 
 import kotlinx.cinterop.*
 import libgrpcpp_c.*
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.ref.createCleaner
 
-@OptIn(ExperimentalForeignApi::class)
+@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
 internal class GrpcByteBuffer internal constructor(
     internal val cByteBuffer: CPointer<grpc_byte_buffer>
-) : AutoCloseable {
+) {
 
     constructor(slice: GrpcSlice) : this(memScoped {
         grpc_raw_byte_buffer_create(slice.cSlice, 1u) ?: error("Failed to create byte buffer")
     })
+
+    init {
+        createCleaner(cByteBuffer) {
+            grpc_byte_buffer_destroy(it)
+        }
+    }
 
     fun intoSlice(): GrpcSlice {
         memScoped {
@@ -22,10 +30,6 @@ internal class GrpcByteBuffer internal constructor(
             grpc_byte_buffer_dump_to_single_slice(cByteBuffer, respSlice.ptr)
             return GrpcSlice(respSlice.readValue())
         }
-    }
-
-    override fun close() {
-        grpc_byte_buffer_destroy(cByteBuffer)
     }
 
 }
--- grpc/grpc-core/src/nativeTest/kotlin/kotlinx/rpc/grpc/internal/ProtoWireTest.kt
+++ grpc/grpc-core/src/nativeTest/kotlin/kotlinx/rpc/grpc/internal/ProtoWireTest.kt
@@ -2,25 +2,26 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.rpc.grpc
+package kotlinx.rpc.grpc.internal
 
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.rpc.grpc.internal.protowire.WireDecoder
 import kotlinx.rpc.grpc.internal.protowire.WireEncoder
 import kotlinx.rpc.grpc.internal.protowire.WireType
+import kotlin.experimental.ExperimentalNativeApi
 import kotlin.test.Test
 
 @OptIn(ExperimentalForeignApi::class)
 class ProtoWireTest {
 
+    @OptIn(ExperimentalNativeApi::class)
     @Test
-    fun testEncodeDecodeBool() {
-
+    fun testEncodeDecodeBool()  {
         val fieldNr = 3;
 
         val encoder = WireEncoder()
-        encoder.write(fieldNr, true)
-        encoder.write(4, "Hello Test")
+        encoder.writeBool(fieldNr, true)
+        encoder.writeString(4, "Hello Test")
 
         val encodedBuffer = encoder.buffer.get()
         val decoder = WireDecoder(encodedBuffer)
@@ -38,7 +39,5 @@ class ProtoWireTest {
 
         val str = decoder.readString()!!
         check(str == "Hello Test")
-
-
     }
 }
