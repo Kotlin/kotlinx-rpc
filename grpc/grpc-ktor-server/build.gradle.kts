95a349b61b0ca76e0d4cf25011f584c5111fdbb2
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-07-23 17:18:38 +0200

    Kmp Source Sets for gRPC (#402)

--- grpc/grpc-ktor-server/build.gradle.kts
+++ grpc/grpc-ktor-server/build.gradle.kts
@@ -2,6 +2,11 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
+import kotlinx.rpc.buf.tasks.BufGenerateTask
+import kotlinx.rpc.proto.kxrpc
+import org.gradle.kotlin.dsl.kotlin
+import org.gradle.kotlin.dsl.withType
+
 plugins {
     alias(libs.plugins.conventions.kmp)
     alias(libs.plugins.kotlinx.rpc)
@@ -13,6 +18,41 @@ kotlin {
             dependencies {
                 api(projects.grpc.grpcCore)
                 implementation(libs.ktor.server.core)
+            }
+        }
+
+        jvmTest {
+            dependencies {
+                implementation(kotlin("test"))
+
+                implementation(projects.grpc.grpcCore)
+                implementation(projects.grpc.grpcKtorServer)
+
+                implementation(libs.grpc.kotlin.stub)
+                implementation(libs.grpc.netty)
+
+                implementation(libs.ktor.server.core)
+                implementation(libs.ktor.server.test.host)
+
+                runtimeOnly(libs.logback.classic)
+            }
+        }
+    }
+}
+
+rpc {
+    grpc {
+        val globalRootDir: String by extra
+
+        protocPlugins.kxrpc {
+            local {
+                javaJar("$globalRootDir/protobuf-plugin/build/libs/protobuf-plugin-$version-all.jar")
+            }
+        }
+
+        project.tasks.withType<BufGenerateTask>().configureEach {
+            if (name.endsWith("Test")) {
+                dependsOn(":protobuf-plugin:jar")
             }
         }
     }
