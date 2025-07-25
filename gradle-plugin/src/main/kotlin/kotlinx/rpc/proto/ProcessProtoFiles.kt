4c7b56d8150d5e03ca2999b3361fdeee19d5328f
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-07-25 12:41:14 +0200

    Tasks caching (#410)

--- gradle-plugin/src/main/kotlin/kotlinx/rpc/proto/ProcessProtoFiles.kt
+++ gradle-plugin/src/main/kotlin/kotlinx/rpc/proto/ProcessProtoFiles.kt
@@ -4,13 +4,15 @@
 
 package kotlinx.rpc.proto
 
+import kotlinx.rpc.util.ensureDirectoryExists
+import kotlinx.rpc.util.ensureRegularFileExists
 import org.gradle.api.Project
 import org.gradle.api.file.SourceDirectorySet
-import org.gradle.api.provider.Provider
 import org.gradle.api.tasks.Copy
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.register
 import java.io.File
+import java.nio.file.Files
 
 /**
  * Copy proto files to a temporary directory for Buf to process.
@@ -23,17 +25,20 @@ public abstract class ProcessProtoFiles : Copy() {
 
 internal fun Project.registerProcessProtoFilesTask(
     name: String,
-    baseGenDir: Provider<File>,
+    destination: File,
     protoFiles: SourceDirectorySet,
-    toDir: String,
     configure: ProcessProtoFiles.() -> Unit = {},
 ): TaskProvider<ProcessProtoFiles> {
     val capitalName = name.replaceFirstChar { it.uppercase() }
 
     return tasks.register<ProcessProtoFiles>("process${capitalName}ProtoFiles") {
-        val protoGenDir = baseGenDir.map { it.resolve(toDir) }
-
         val allFiles = protoFiles.files
+
+        // this task deletes the destination directory if it results in NO-SOURCE,
+        // which breaks the configuration cache for bufGenerate
+        // so we prevent No-SOURCE by creating a .keep file in the destination directory
+        val keep = protoBuildDirSourceSetsKeep.ensureRegularFileExists()
+        from(keep)
 
         from(protoFiles.srcDirs) {
             include {
@@ -41,10 +46,10 @@ internal fun Project.registerProcessProtoFilesTask(
             }
         }
 
-        into(protoGenDir)
+        into(destination)
 
         doFirst {
-            protoGenDir.get().deleteRecursively()
+            destination.deleteRecursively()
         }
 
         configure()
