a022cbcef882e9c50d13159929604d5bfb35cc1b
Johannes Zottele <official@johannes-zottele.at>
2025-07-29 12:03:27 +0200

    gRPC/Native: Add Wire Format encoder and decoder (#412)
    Co-authored-by: Alexander Sysoev <Alexander.Sysoev@jetbrains.com>

---
+++ grpc/grpc-core/src/jsMain/kotlin/kotlinx/rpc/grpc/internal/WireEncoder.js.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc.internal
+
+import kotlinx.io.Sink
+
+internal actual fun WireEncoder(sink: Sink): WireEncoder {
+    TODO("Not yet implemented")
+}
