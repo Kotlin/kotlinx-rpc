8e1fdf5038d331bab73a6d992640a634b5e263bf
Johannes Zottele <official@johannes-zottele.at>
2025-07-29 12:03:27 +0200

    gRPC/Native: Add Wire Format encoder and decoder (#412)
    Co-authored-by: Alexander Sysoev <Alexander.Sysoev@jetbrains.com>

--- grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcByteBuffer.kt
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcByteBuffer.kt
@@ -2,19 +2,27 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.rpc.grpc.bridge
+package kotlinx.rpc.grpc.internal.bridge
 
 import kotlinx.cinterop.*
 import libgrpcpp_c.*
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.ref.createCleaner
 
-@OptIn(ExperimentalForeignApi::class)
+@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
 internal class GrpcByteBuffer internal constructor(
     internal val cByteBuffer: CPointer<grpc_byte_buffer>
-) : AutoCloseable {
+) {
 
     constructor(slice: GrpcSlice) : this(memScoped {
         grpc_raw_byte_buffer_create(slice.cSlice, 1u) ?: error("Failed to create byte buffer")
     })
+
+    init {
+        createCleaner(cByteBuffer) {
+            grpc_byte_buffer_destroy(it)
+        }
+    }
 
     fun intoSlice(): GrpcSlice {
         memScoped {
@@ -22,10 +30,6 @@ internal class GrpcByteBuffer internal constructor(
             grpc_byte_buffer_dump_to_single_slice(cByteBuffer, respSlice.ptr)
             return GrpcSlice(respSlice.readValue())
         }
-    }
-
-    override fun close() {
-        grpc_byte_buffer_destroy(cByteBuffer)
     }
 
 }
--- grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcSlice.kt
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/bridge/GrpcSlice.kt
@@ -2,7 +2,7 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.rpc.grpc.bridge
+package kotlinx.rpc.grpc.internal.bridge
 
 import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
@@ -11,9 +11,11 @@ import kotlinx.cinterop.usePinned
 import libgrpcpp_c.grpc_slice
 import libgrpcpp_c.grpc_slice_from_copied_buffer
 import libgrpcpp_c.grpc_slice_unref
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.ref.createCleaner
 
-@OptIn(ExperimentalForeignApi::class)
-internal class GrpcSlice internal constructor(internal val cSlice: CValue<grpc_slice>) : AutoCloseable {
+@OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
+internal class GrpcSlice internal constructor(internal val cSlice: CValue<grpc_slice>) {
 
     constructor(buffer: ByteArray) : this(
         buffer.usePinned { pinned ->
@@ -21,7 +23,9 @@ internal class GrpcSlice internal constructor(internal val cSlice: CValue<grpc_s
         }
     )
 
-    override fun close() {
-        grpc_slice_unref(cSlice)
+    init {
+        createCleaner(cSlice) {
+            grpc_slice_unref(it)
+        }
     }
 }
