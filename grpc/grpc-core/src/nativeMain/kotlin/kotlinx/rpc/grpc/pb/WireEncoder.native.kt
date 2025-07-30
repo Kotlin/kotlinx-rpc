47ea8a00c391a39b809b9811d4700ca3bcfe7244
Johannes Zottele <official@johannes-zottele.at>
2025-07-30 16:51:20 +0200

    gRPC: Support initial proto generation for common (JVM and Native) (#415)
    * grpc-native: First message generation
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Support for repeated fields in Message generation
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Remove guards in generated kotlin
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Move internal decode/encoding into grpc.pb package and move common proto generation to commonTest
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Address PR comments
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Rename ProtosTest class
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/pb/WireEncoder.native.kt
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/pb/WireEncoder.native.kt
@@ -2,10 +2,11 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.rpc.grpc.internal
+package kotlinx.rpc.grpc.pb
 
 import kotlinx.cinterop.*
 import kotlinx.io.Sink
+import kotlinx.rpc.grpc.internal.writeFully
 import libprotowire.*
 import kotlin.experimental.ExperimentalNativeApi
 import kotlin.native.ref.createCleaner
