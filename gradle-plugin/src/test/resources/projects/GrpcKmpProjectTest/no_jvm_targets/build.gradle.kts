fba74a8d4c4da4ea9d6907b192c0aafe4f1c4e69
Johannes Zottele <official@johannes-zottele.at>
2025-08-01 15:56:21 +0200

    protoc-plugin: Refactoring of intermediate model  (#418)

--- gradle-plugin/src/test/resources/projects/GrpcKmpProjectTest/no_jvm_targets/build.gradle.kts
+++ gradle-plugin/src/test/resources/projects/GrpcKmpProjectTest/no_jvm_targets/build.gradle.kts
@@ -16,6 +16,6 @@ kotlin {
 
 // should nonetheless be available
 protoSourceSets {
-    jvmMain {}
-    jvmTest {}
+    commonMain {}
+    commonTest {}
 }
--- protoc-gen/src/main/kotlin/kotlinx/rpc/protobuf/model/Model.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
-
-package kotlinx.rpc.protobuf.model
-
-data class Model(
-    val files: List<FileDeclaration>,
-)
