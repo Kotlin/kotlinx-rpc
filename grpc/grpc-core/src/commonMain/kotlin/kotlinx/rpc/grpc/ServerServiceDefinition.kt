be50bb13609c9a548405ac7f61eb7693b937d887
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-08-01 16:53:41 +0200

    Grpc common APIs for Services, Client and Server (#419)

--- grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ServerServiceDefinition.kt
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ServerServiceDefinition.kt
@@ -6,7 +6,20 @@
 
 package kotlinx.rpc.grpc
 
+import kotlinx.rpc.grpc.internal.ServerMethodDefinition
+import kotlinx.rpc.grpc.internal.ServiceDescriptor
+import kotlinx.rpc.internal.utils.InternalRpcApi
+
 /**
  * Definition of a service to be exposed via a Server.
  */
-public expect class ServerServiceDefinition
+public expect class ServerServiceDefinition {
+    public fun getServiceDescriptor(): ServiceDescriptor
+    public fun getMethods(): Collection<ServerMethodDefinition<*, *>>
+}
+
+@InternalRpcApi
+public expect fun serverServiceDefinition(
+    serviceDescriptor: ServiceDescriptor,
+    methods: Collection<ServerMethodDefinition<*, *>>
+): ServerServiceDefinition
---
+++ grpc/grpc-core/src/nativeMain/kotlin/kotlinx/rpc/grpc/internal/ServerCall.native.kt
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc.internal
+
+import kotlinx.rpc.grpc.GrpcTrailers
+import kotlinx.rpc.grpc.Status
+import kotlinx.rpc.internal.utils.InternalRpcApi
+
+@InternalRpcApi
+public actual fun interface ServerCallHandler<Request, Response> {
+    public actual fun startCall(
+        call: ServerCall<Request, Response>,
+        headers: GrpcTrailers,
+    ): ServerCall.Listener<Request>
+}
+
+@InternalRpcApi
+public actual abstract class ServerCall<Request, Response> {
+    public actual abstract fun request(numMessages: Int)
+    public actual abstract fun sendHeaders(headers: GrpcTrailers)
+    public actual abstract fun sendMessage(message: Response)
+    public actual abstract fun close(status: Status, trailers: GrpcTrailers)
+
+    public actual open fun isReady(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    public actual abstract fun isCancelled(): Boolean
+    public actual abstract fun getMethodDescriptor(): MethodDescriptor<Request, Response>
+
+    @InternalRpcApi
+    public actual abstract class Listener<Request> {
+        public actual open fun onMessage(message: Request) {}
+        public actual open fun onHalfClose() {}
+        public actual open fun onCancel() {}
+        public actual open fun onComplete() {}
+        public actual open fun onReady() {}
+    }
+}
+
+@InternalRpcApi
+public actual fun <State, Message> serverCallListener(
+    state: State,
+    onMessage: (State, message: Message) -> Unit,
+    onHalfClose: (State) -> Unit,
+    onCancel: (State) -> Unit,
+    onComplete: (State) -> Unit,
+    onReady: (State) -> Unit,
+): ServerCall.Listener<Message> {
+    TODO("Not yet implemented")
+}
