acda7b26d0deb8572865a9e91da14253454276d3
Johannes Zottele <official@johannes-zottele.at>
2025-08-07 11:00:05 +0200

    grpc: Support sub-messages (#430)
    * grpc-pb: Implement sub messages
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: First successful sub-message test
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Support recursive messages
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Support nested message
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Support repeated messages
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Make generated methods internal
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Support message in oneof
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Add test for enum in oneOf
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Add oneOf message merge test
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Check required fields ins submessages
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Address PR comments
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/commonTest/kotlin/kotlinx/rpc/grpc/pb/ProtosTest.kt
+++ grpc/grpc-core/src/commonTest/kotlin/kotlinx/rpc/grpc/pb/ProtosTest.kt
@@ -6,22 +6,30 @@ package kotlinx.rpc.grpc.pb
 
 import OneOfMsg
 import OneOfMsgInternal
+import OneOfWithRequired
+import Outer
+import OuterInternal
+import asInternal
+import encodeWith
 import invoke
 import kotlinx.io.Buffer
 import kotlinx.rpc.grpc.codec.MessageCodec
-import kotlinx.rpc.grpc.test.Enum
+import kotlinx.rpc.grpc.test.MyEnum
 import kotlinx.rpc.grpc.test.UsingEnum
 import kotlinx.rpc.grpc.test.UsingEnumInternal
 import kotlinx.rpc.grpc.test.common.*
 import kotlinx.rpc.grpc.test.invoke
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertFailsWith
-import kotlin.test.assertNull
+import test.nested.*
+import test.recursive.Recursive
+import test.recursive.RecursiveInternal
+import test.recursive.RecursiveReq
+import test.recursive.invoke
+import test.submsg.*
+import kotlin.test.*
 
 class ProtosTest {
 
-    private fun <M> decodeEncode(
+    private fun <M> encodeDecode(
         msg: M,
         codec: MessageCodec<M>
     ): M {
@@ -51,33 +59,49 @@ class ProtosTest {
 
         val msgObj = msg
 
-        val decoded = decodeEncode(msgObj, AllPrimitivesInternal.CODEC)
+        val decoded = encodeDecode(msgObj, AllPrimitivesInternal.CODEC)
 
         assertEquals(msg.double, decoded.double)
     }
 
     @Test
     fun testRepeatedProto() {
+        val elem = { i: Int -> Repeated.Other { a = i } }
         val msg = Repeated {
             listFixed32 = listOf(1, 5, 3).map { it.toUInt() }
             listFixed32Packed = listOf(1, 2, 3).map { it.toUInt() }
             listInt32 = listOf(4, 7, 6)
             listInt32Packed = listOf(4, 5, 6)
             listString = listOf("a", "b", "c")
+            listMessage = listOf(elem(1), elem(2), elem(3))
         }
 
-        val decoded = decodeEncode(msg, RepeatedInternal.CODEC)
+        val decoded = encodeDecode(msg, RepeatedInternal.CODEC)
 
         assertEquals(msg.listInt32, decoded.listInt32)
         assertEquals(msg.listFixed32, decoded.listFixed32)
         assertEquals(msg.listString, decoded.listString)
+        assertEquals(msg.listMessage.size, decoded.listMessage.size)
+        for (i in msg.listMessage.indices) {
+            assertEquals(msg.listMessage[i].a, decoded.listMessage[i].a)
+        }
+    }
+
+    @Test
+    fun testRepeatedWithRequiredSubField() {
+        assertFailsWith<IllegalStateException> {
+            RepeatedWithRequired {
+                // we construct the message using the internal class,
+                // so it is not invoking the checkRequired method on construction
+                msgList = listOf(PresenceCheck { RequiredPresence = 2 }, PresenceCheckInternal())
+            }
+        }
     }
 
     @Test
     fun testPresenceCheckProto() {
-
         // Check a missing required field in a user-constructed message
-        assertFailsWith<IllegalStateException>("PresenceCheck is missing required field: RequiredPresence") {
+        assertFailsWith<IllegalStateException> {
             PresenceCheck {}
         }
 
@@ -87,7 +111,7 @@ class ProtosTest {
         encoder.writeFloat(2, 1f)
         encoder.flush()
 
-        assertFailsWith<IllegalStateException>("PresenceCheck is missing required field: RequiredPresence") {
+        assertFailsWith<IllegalStateException> {
             PresenceCheckInternal.CODEC.decode(buffer)
         }
     }
@@ -101,18 +125,18 @@ class ProtosTest {
         encoder.flush()
 
         val decodedMsg = UsingEnumInternal.CODEC.decode(buffer)
-        assertEquals(Enum.UNRECOGNIZED(50), decodedMsg.enum)
+        assertEquals(MyEnum.UNRECOGNIZED(50), decodedMsg.enum)
     }
 
     @Test
     fun testEnumAlias() {
         val msg = UsingEnum {
-            enum = Enum.ONE_SECOND
+            enum = MyEnum.ONE_SECOND
         }
 
-        val decodedMsg = decodeEncode(msg, UsingEnumInternal.CODEC)
-        assertEquals(Enum.ONE, decodedMsg.enum)
-        assertEquals(Enum.ONE_SECOND, decodedMsg.enum)
+        val decodedMsg = encodeDecode(msg, UsingEnumInternal.CODEC)
+        assertEquals(MyEnum.ONE, decodedMsg.enum)
+        assertEquals(MyEnum.ONE_SECOND, decodedMsg.enum)
     }
 
     @Test
@@ -125,22 +149,69 @@ class ProtosTest {
         assertEquals(0, buffer.size)
 
         val decoded = UsingEnumInternal.CODEC.decode(buffer)
-        assertEquals(Enum.ZERO, decoded.enum)
+        assertEquals(MyEnum.ZERO, decoded.enum)
     }
 
     @Test
     fun testOneOf() {
-        val msg1 = OneOfMsg {
-            field = OneOfMsg.Field.Sint(23)
+        run {
+            val msg = OneOfMsg {
+                field = OneOfMsg.Field.Sint(23)
+            }
+            val decoded = encodeDecode(msg, OneOfMsgInternal.CODEC)
+            assertEquals(OneOfMsg.Field.Sint(23), decoded.field)
         }
-        val decoded1 = decodeEncode(msg1, OneOfMsgInternal.CODEC)
-        assertEquals(OneOfMsg.Field.Sint(23), decoded1.field)
 
-        val msg2 = OneOfMsg {
-            field = OneOfMsg.Field.Fixed(21u)
+        run {
+            val msg = OneOfMsg {
+                field = OneOfMsg.Field.Fixed(21u)
+            }
+            val decoded = encodeDecode(msg, OneOfMsgInternal.CODEC)
+            assertEquals(OneOfMsg.Field.Fixed(21u), decoded.field)
         }
-        val decoded2 = decodeEncode(msg2, OneOfMsgInternal.CODEC)
-        assertEquals(OneOfMsg.Field.Fixed(21u), decoded2.field)
+
+        run {
+            val msg = OneOfMsg {
+                field = OneOfMsg.Field.Other(Other { arg2 = "test" })
+            }
+            val decoded = encodeDecode(msg, OneOfMsgInternal.CODEC)
+            assertIs<OneOfMsg.Field.Other>(decoded.field)
+            assertNull((decoded.field as OneOfMsg.Field.Other).value.arg1)
+            assertEquals("test", (decoded.field as OneOfMsg.Field.Other).value.arg2)
+            assertNull((decoded.field as OneOfMsg.Field.Other).value.arg3)
+        }
+
+        run {
+            val msg = OneOfMsg {
+                field = OneOfMsg.Field.Enum(MyEnum.ONE_SECOND)
+            }
+            val decoded = encodeDecode(msg, OneOfMsgInternal.CODEC)
+            assertEquals(MyEnum.ONE, (decoded.field as OneOfMsg.Field.Enum).value)
+        }
+    }
+
+    @Test
+    fun testOneOfMsgMerging() {
+        val part1 = OneOfMsg {
+            field = OneOfMsg.Field.Other(Other { arg2 = "arg2" })
+        }
+        val part2 = OneOfMsg {
+            field = OneOfMsg.Field.Other(Other { arg1 = "arg1" })
+        }
+
+        val buffer = Buffer()
+        val encoder = WireEncoder(buffer)
+        part1.asInternal().encodeWith(encoder)
+        part2.asInternal().encodeWith(encoder)
+        encoder.flush()
+
+
+        val decoded = OneOfMsgInternal.CODEC.decode(buffer)
+        assertIs<OneOfMsg.Field.Other>(decoded.field)
+        val decodedOther = (decoded.field as OneOfMsg.Field.Other).value
+        assertEquals("arg2", decodedOther.arg2)
+        assertEquals("arg1", decodedOther.arg1)
+        assertEquals(null, decodedOther.arg3)
     }
 
     @Test
@@ -158,12 +229,132 @@ class ProtosTest {
     }
 
     @Test
+    fun testOneOfRequiredSubField() {
+        assertFailsWith<IllegalStateException> {
+            OneOfWithRequired {
+                // we construct the message using the internal class,
+                // so it is not invoking the checkRequired method on construction
+                field = OneOfWithRequired.Field.Msg(PresenceCheckInternal())
+            }
+        }
+    }
+
+    @Test
     fun testOneOfNull() {
         // write two values on the oneOf field.
         // the second value must be the one stored during decoding.
         val buffer = Buffer()
         val decoded = OneOfMsgInternal.CODEC.decode(buffer)
         assertNull(decoded.field)
+    }
+
+    @Test
+    fun testSubMessage() {
+        val msg = Outer {
+            inner = Outer.Inner {
+                field = 12345678
+            }
+        }
+        val decoded = encodeDecode(msg, OuterInternal.CODEC)
+        assertEquals(msg.inner.field, decoded.inner.field)
+    }
+
+    @Test
+    fun testRecursiveReqNotSet() {
+        assertFailsWith<IllegalStateException> {
+            val msg = RecursiveReq {
+                rec = RecursiveReq {
+                    rec = RecursiveReq {
+
+                    }
+                    num = 3
+                }
+            }
+        }
+    }
+
+    @Test
+    fun testRecursive() {
+        val msg = Recursive {
+            rec = Recursive {
+                rec = Recursive {}
+                num = 3
+            }
+        }
+
+        assertEquals(null, msg.rec.rec.rec.rec.num)
+        assertEquals(3, msg.rec.num)
+
+        val decoded = encodeDecode(msg, RecursiveInternal.CODEC)
+
+        assertEquals(3, decoded.rec.num)
+        assertEquals(null, decoded.rec.rec.rec.rec.num)
+    }
+
+    @Test
+    fun testNested() {
+        val inner = NestedOuter.Inner.SuperInner.DuperInner.EvenMoreInner.CantBelieveItsSoInner {
+            num = 123456789
+        }
+
+        val notInside = NotInside {
+            num = -12
+        }
+        val outer = NestedOuter {
+            deep = inner
+            deepEnum =
+                NestedOuter.Inner.SuperInner.DuperInner.EvenMoreInner.JustWayTooInner.JUST_WAY_TOO_INNER_UNSPECIFIED
+        }
+
+        assertEquals(123456789, outer.deep.num)
+        assertEquals(
+            NestedOuter.Inner.SuperInner.DuperInner.EvenMoreInner.JustWayTooInner.JUST_WAY_TOO_INNER_UNSPECIFIED,
+            outer.deepEnum
+        )
+        assertEquals(-12, notInside.num)
+
+        val decodedOuter = encodeDecode(outer, NestedOuterInternal.CODEC)
+        assertEquals(123456789, decodedOuter.deep.num)
+        assertEquals(
+            NestedOuter.Inner.SuperInner.DuperInner.EvenMoreInner.JustWayTooInner.JUST_WAY_TOO_INNER_UNSPECIFIED,
+            decodedOuter.deepEnum
+        )
+        assertEquals(-12, notInside.num)
+
+        val decodedNotInside = encodeDecode(notInside, NotInsideInternal.CODEC)
+        assertEquals(-12, decodedNotInside.num)
+
+        val decodedInner = encodeDecode(
+            inner,
+            NestedOuterInternal.InnerInternal.SuperInnerInternal.DuperInnerInternal.EvenMoreInnerInternal.CantBelieveItsSoInnerInternal.CODEC
+        )
+        assertEquals(123456789, decodedInner.num)
+    }
+
+    @Test
+    fun testMessageMerging() {
+
+        val buffer = Buffer()
+        val encoder = WireEncoder(buffer)
+
+        val firstPart = Other {
+            arg1 = "first"
+            arg2 = "second"
+        }
+        val secondPart = Other {
+            arg2 = "third"
+            arg3 = "fourth"
+        }
+
+        encoder.writeMessage(1, firstPart as OtherInternal) { encodeWith(encoder) }
+        encoder.flush()
+        encoder.writeMessage(1, secondPart as OtherInternal) { encodeWith(encoder) }
+        encoder.flush()
+
+        val decoded = ReferenceInternal.CODEC.decode(buffer)
+        assertEquals("first", decoded.other.arg1)
+        assertEquals("third", decoded.other.arg2)
+        assertEquals("fourth", decoded.other.arg3)
     }
 
 }
