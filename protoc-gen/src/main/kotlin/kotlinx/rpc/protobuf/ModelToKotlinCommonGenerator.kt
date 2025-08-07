87b41adb8bddf1b7f70c3a2fa774dea26bca0f81
Johannes Zottele <official@johannes-zottele.at>
2025-08-07 16:06:48 +0200

    grpc-pb: Add InvalidProtobufError class
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- protoc-gen/src/main/kotlin/kotlinx/rpc/protobuf/ModelToKotlinCommonGenerator.kt
+++ protoc-gen/src/main/kotlin/kotlinx/rpc/protobuf/ModelToKotlinCommonGenerator.kt
@@ -586,7 +586,7 @@ class ModelToKotlinCommonGenerator(
 
         requiredFields.forEach { field ->
             ifBranch(condition = "!presenceMask[${field.presenceIdx}]", ifBlock = {
-                code("error(\"${declaration.name.simpleName} is missing required field: ${field.name}\")")
+                code("throw kotlinx.rpc.grpc.InvalidProtobufError.missingRequiredField(\"${declaration.name.simpleName}\", \"${field.name}\")")
             })
         }
 
