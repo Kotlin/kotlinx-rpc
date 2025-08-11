74a49d961445b3bc2e5354e7187bb63db70a9503
Johannes Zottele <official@johannes-zottele.at>
2025-08-11 18:49:10 +0200

    grpc-pb: Add gRPC Exceptions and skip unknown fields (#437)
    * grpc-pb: Skip unknown fields
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Add InvalidProtobufError class
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Throw error in decoder instead of hadError() check
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Fix string encoding bug
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Remove hadError() method
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Throw exception instead of Boolean when encoding value
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Move JVM exception check to CODEC to avoid performance overhead
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Address PR comments
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-pb: Address PR Comments
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

---
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/pb/ProtobufException.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.grpc.pb
+
+public sealed class ProtobufException : RuntimeException {
+    protected constructor(message: String, cause: Throwable? = null) : super(message, cause)
+}
+
+
+public class ProtobufDecodingException : ProtobufException {
+    public constructor(message: String, cause: Throwable? = null) : super(message, cause)
+
+    public companion object Companion {
+        internal fun missingRequiredField(messageName: String, fieldName: String) =
+            ProtobufDecodingException("Message '$messageName' is missing a required field: $fieldName")
+
+        internal fun negativeSize() = ProtobufDecodingException(
+            "Decoder encountered an embedded string or message which claimed to have negative size."
+        )
+
+        internal fun invalidTag() = ProtobufDecodingException(
+            "Protocol message contained an invalid tag (zero)."
+        )
+
+        internal fun truncatedMessage() = ProtobufDecodingException(
+            ("While parsing a protocol message, the input ended unexpectedly "
+                    + "in the middle of a field.  This could mean either that the "
+                    + "input has been truncated or that an embedded message "
+                    + "misreported its own length.")
+        )
+
+        internal fun genericParsingError() = ProtobufDecodingException("Failed to parse the message.")
+    }
+}
+
+public class ProtobufEncodingException : ProtobufException {
+    public constructor(message: String, cause: Throwable? = null) : super(message, cause)
+}
