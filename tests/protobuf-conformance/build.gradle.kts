2ab9953d374906a8eca0343a97d37f133f610b93
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-08-14 21:55:52 +0200

    added running conformance tests

--- tests/protobuf-conformance/build.gradle.kts
+++ tests/protobuf-conformance/build.gradle.kts
@@ -7,7 +7,7 @@
 import kotlinx.rpc.buf.tasks.BufGenerateTask
 import kotlinx.rpc.internal.InternalRpcApi
 import kotlinx.rpc.internal.configureLocalProtocGenDevelopmentDependency
-import kotlinx.rpc.protoc.ProcessProtoFiles
+import org.jetbrains.kotlin.gradle.dsl.ExplicitApiMode
 import util.tasks.setupProtobufConformanceResources
 
 plugins {
@@ -15,32 +15,82 @@ plugins {
     alias(libs.plugins.kotlinx.rpc)
 }
 
+kotlin {
+    explicitApi = ExplicitApiMode.Disabled
+}
+
 dependencies {
-    testImplementation(projects.grpc.grpcCodec)
-    testImplementation(projects.protobuf.protobufCore)
+    implementation(libs.coroutines.core)
+    implementation(libs.kotlin.reflect)
+    implementation(projects.grpc.grpcCodec)
+    implementation(projects.protobuf.protobufCore)
+
+    testImplementation(libs.kotlin.test.junit5)
+    testImplementation(libs.coroutines.test)
 }
 
 setupProtobufConformanceResources()
-configureLocalProtocGenDevelopmentDependency()
+configureLocalProtocGenDevelopmentDependency("Main")
 
 val generatedCodeDir = project.layout.projectDirectory
     .dir("src")
-    .dir("test")
+    .dir("main")
     .dir("generated-code")
     .asFile
 
 tasks.withType<BufGenerateTask>().configureEach {
-    if (name.endsWith("Test")) {
+    if (name.endsWith("Main")) {
         outputDirectory = generatedCodeDir
     }
 }
 
 protoSourceSets {
-    test {
+    main {
         proto {
             exclude("**/test_messages_proto2.proto")
             exclude("**/test_messages_proto2_editions.proto")
             exclude("**/test_messages_edition2023.proto")
         }
     }
+}
+
+val mockClientJar = tasks.register<Jar>("mockClientJar") {
+    archiveBaseName.set("mockClient")
+    archiveVersion.set("")
+
+    manifest {
+        attributes["Main-Class"] = "kotlinx.rpc.protoc.gen.test.ConformanceClientKt"
+    }
+
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    from(
+        configurations.runtimeClasspath.map { prop ->
+            prop.map { if (it.isDirectory()) it else zipTree(it) }
+        }
+    )
+
+    with(tasks.jar.get())
+}
+
+
+val generateConformanceTests = tasks.register<JavaExec>("generateConformanceTests") {
+    classpath = sourceSets.main.get().runtimeClasspath
+
+    dependsOn(mockClientJar)
+    dependsOn(tasks.named("bufGenerateMain"))
+
+    args = listOf(
+        mockClientJar.get().archiveFile.get().asFile.absolutePath
+    )
+
+    mainClass.set("kotlinx.rpc.protoc.gen.test.GenerateConformanceTestsKt")
+}
+
+
+tasks.test {
+    environment("MOCK_CLIENT_JAR", mockClientJar.get().archiveFile.get().asFile.absolutePath)
+
+    useJUnitPlatform()
+
+    dependsOn(generateConformanceTests)
 }
---
+++ tests/protobuf-conformance/src/main/kotlin/kotlinx/rpc/protoc/gen/test/runner/runner.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+package kotlinx.rpc.protoc.gen.test.runner
+
+import CONFORMANCE_EXECUTABLE_PATH
+import java.nio.file.Files
+import java.nio.file.Path
+import kotlin.io.path.createFile
+import kotlin.io.path.createParentDirectories
+import kotlin.io.path.deleteIfExists
+import kotlin.io.path.readLines
+
+class RunResult(
+    val exitCode: Int,
+    val stdout: List<String>,
+    val stderr: List<String>,
+)
+
+fun execConformanceTestRunner(
+    outputDir: Path,
+    failingTestsFile: Path?,
+    textFormatFailingTestsFile: Path?,
+    executable: String,
+): Result<RunResult> {
+    val stdoutStream = Files.createTempFile("stdout", ".log")
+    val errorStream = Files.createTempFile("error", ".log")
+
+    return runCatching {
+        val process = ProcessBuilder(
+            CONFORMANCE_EXECUTABLE_PATH,
+            "--maximum_edition", "MAX",
+            "--enforce_recommended",
+            "--failure_list", failingTestsFile.toString(),
+            "--text_format_failure_list", textFormatFailingTestsFile.toString(),
+            "--output_dir",
+            outputDir.toString(),
+            executable,
+        ).redirectError(errorStream.toFile())
+            .redirectOutput(stdoutStream.toFile())
+
+        val exitCode = process.start().waitFor()
+
+        RunResult(
+            exitCode = exitCode,
+            stdout = stdoutStream.readLines(),
+            stderr = errorStream.readLines(),
+        )
+    }
+}
+
+fun createConformanceTestFiles(
+    outputDir: Path,
+    createBlank: Boolean = true,
+): Pair<Path, Path> {
+    val failingTestsFile = outputDir.resolve("failing_tests.txt").apply {
+        createParentDirectories()
+        if (createBlank) {
+            deleteIfExists()
+            createFile()
+        }
+    }
+
+    val textFormatFailingTestsFile = outputDir.resolve("text_format_failing_tests.txt").apply {
+        createParentDirectories()
+        if (createBlank) {
+            deleteIfExists()
+            createFile()
+        }
+    }
+
+    return failingTestsFile to textFormatFailingTestsFile
+}
