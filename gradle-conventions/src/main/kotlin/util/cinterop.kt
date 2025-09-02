c7f445b46b4e7e63fa84e0f5d49a1529bf097d12
Johannes Zottele <official@johannes-zottele.at>
2025-09-02 16:04:29 +0200

    grpc-native: Add linux support (#466)
    * grpc-native: Add linux support
    
    * grpc-native: Add support for linux
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    * grpc-native: Half working with konan (only kgrpc is not compiling)
    
    * grpc-native: Working protobuf konan compile
    
    * grpc-native: Little fix
    
    * grpc-native: Little fix
    
    * grpc-native: Not working state
    
    * grpc-native: Working linux arm64 and x64
    
    * grpc-native: Address PR comments
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>
    
    ---------
    
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- gradle-conventions/src/main/kotlin/util/cinterop.kt
+++ gradle-conventions/src/main/kotlin/util/cinterop.kt
@@ -12,13 +12,16 @@ import org.gradle.api.tasks.Exec
 import org.gradle.api.tasks.TaskContainer
 import org.gradle.api.tasks.TaskProvider
 import org.gradle.internal.extensions.stdlib.capitalized
-import org.gradle.kotlin.dsl.*
+import org.gradle.kotlin.dsl.extra
+import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.register
+import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 import org.jetbrains.kotlin.gradle.plugin.mpp.DefaultCInteropSettings
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.tasks.CInteropProcess
-import org.jetbrains.kotlin.konan.target.Family
-import org.jetbrains.kotlin.konan.target.KonanTarget
+import util.other.libs
 import java.io.File
 
 // works with the cinterop-c Bazel project
@@ -154,12 +157,12 @@ private fun TaskContainer.registerBuildClibTask(
     target: KotlinNativeTarget,
 ): TaskProvider<Exec> {
     return register<Exec>(name) {
-        val platform = target.bazelPlatformName
-        val os = target.bazelOsName
+        val konanTarget = target.konanTarget.visibleName
+        val konanHome = project.findKonanHome()
 
         group = "build"
         workingDir = project.cinteropLibDir
-        commandLine("bash", "-c", "./build_target.sh $bazelTask $outFile $platform $os")
+        commandLine("bash", "-c", "./build_target.sh $bazelTask $outFile $konanTarget $konanHome")
         inputs.files(project.fileTree(project.cinteropLibDir) { exclude("bazel-*/**", "prebuilt-deps/**") })
         outputs.files(outFile)
 
@@ -173,47 +176,22 @@ private val Project.cinteropLibDir: File
         return layout.projectDirectory.dir("$globalRootDir/cinterop-c").asFile.absoluteFile
     }
 
-/**
- * Returns the Bazel platform name for the given [KotlinNativeTarget].
- *
- * For Apple targets, compare the following two lists:
- * - https://kotlinlang.org/docs/native-target-support.html
- * - https://github.com/bazelbuild/apple_support/blob/master/configs/platforms.bzl
- */
-private val KotlinNativeTarget.bazelPlatformName: String
-    get() {
-        val appleSupport = "@build_bazel_apple_support//platforms"
-        return when (konanTarget) {
-            KonanTarget.MACOS_ARM64 -> "$appleSupport:macos_arm64"
-            KonanTarget.MACOS_X64 -> "$appleSupport:macos_x86_64"
-            KonanTarget.IOS_ARM64 -> "$appleSupport:ios_arm64"
-            KonanTarget.IOS_SIMULATOR_ARM64 -> "$appleSupport:ios_sim_arm64"
-            KonanTarget.IOS_X64 -> "$appleSupport:ios_x86_64"
-            KonanTarget.WATCHOS_ARM32 -> "$appleSupport:watchos_armv7k"
-            // WATCHOS_ARM64 is the "older" arm64_32 target, not arm64 (which is WATCH_DEVICE_ARM64)
-            KonanTarget.WATCHOS_ARM64 -> "$appleSupport:watchos_arm64_32"
-            KonanTarget.WATCHOS_DEVICE_ARM64 -> "$appleSupport:watchos_device_arm64"
-            KonanTarget.WATCHOS_SIMULATOR_ARM64 -> "$appleSupport:watchos_arm64"
-            KonanTarget.WATCHOS_X64 -> "$appleSupport:watchos_x86_64"
-            KonanTarget.TVOS_ARM64 -> "$appleSupport:tvos_arm64"
-            KonanTarget.TVOS_SIMULATOR_ARM64 -> "$appleSupport:tvos_sim_arm64"
-            KonanTarget.TVOS_X64 -> "$appleSupport:tvos_x86_64"
-            KonanTarget.LINUX_ARM32_HFP -> TODO()
-            KonanTarget.LINUX_ARM64 -> TODO()
-            KonanTarget.LINUX_X64 -> TODO()
-            KonanTarget.ANDROID_ARM32 -> TODO()
-            KonanTarget.ANDROID_ARM64 -> TODO()
-            KonanTarget.ANDROID_X64 -> TODO()
-            KonanTarget.ANDROID_X86 -> TODO()
-            KonanTarget.MINGW_X64 -> TODO()
+private fun Project.findKonanHome(): String {
+    val userHome = System.getProperty("user.home")
+    val osName = System.getProperty("os.name").lowercase()
+    val hostOs = when {
+        osName.contains("mac") -> "macos"
+        osName.contains("linux") -> "linux"
+        osName.contains("windows") -> "windows"
+        else -> error("Unsupported OS: $osName")
+    }
+    val hostArch = System.getProperty("os.arch").lowercase().let {
+        when {
+            it.contains("aarch64") || it.contains("arm64") -> "aarch64"
+            it.contains("x86_64") || it.contains("amd64") -> "x64"
+            else -> error("Unsupported arch: $it")
         }
     }
-
-private val KotlinNativeTarget.bazelOsName
-    get() = when (konanTarget.family) {
-        Family.OSX -> "macos"
-        Family.IOS -> "ios"
-        Family.TVOS -> "tvos"
-        Family.WATCHOS -> "watchos"
-        else -> TODO()
-    }
+    val kotlinVersion = project.libs.versions.kotlin.compiler.get()
+    return "$userHome/.konan/kotlin-native-prebuilt-$hostOs-$hostArch-$kotlinVersion"
+}
