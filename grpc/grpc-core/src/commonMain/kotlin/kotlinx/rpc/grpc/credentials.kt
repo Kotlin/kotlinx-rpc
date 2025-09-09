0ea6ce753fc943738b6f556dd19773513d76f815
Johannes Zottele <official@johannes-zottele.at>
2025-09-09 13:57:51 +0200

    grpc-native: Fix wrong comment
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/credentials.kt
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/credentials.kt
@@ -2,9 +2,7 @@
  * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
  */
 
-package kotlinx.rpc.grpc/*
- * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
- */
+package kotlinx.rpc.grpc
 
 public expect abstract class ClientCredentials
 public expect abstract class ServerCredentials
