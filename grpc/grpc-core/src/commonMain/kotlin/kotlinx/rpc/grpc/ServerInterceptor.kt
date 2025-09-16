4a62ed29ab8e7b7084950abda28ec5863a989dc7
Johannes Zottele <official@johannes-zottele.at>
2025-09-16 18:27:23 +0200

    grpc: Refactor server scope API
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ServerInterceptor.kt
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ServerInterceptor.kt
@@ -5,21 +5,28 @@
 package kotlinx.rpc.grpc
 
 import kotlinx.coroutines.flow.Flow
-import kotlinx.rpc.grpc.internal.GrpcCallOptions
+import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.rpc.grpc.internal.MethodDescriptor
 
 public interface ServerCallScope<Request, Response> {
     public val method: MethodDescriptor<Request, Response>
+    public val requestHeaders: GrpcTrailers
     public val responseHeaders: GrpcTrailers
-    public fun onCancel(block: () -> Unit)
-    public fun onComplete(block: () -> Unit)
+    public val responseTrailers: GrpcTrailers
+
+    public fun onClose(block: (Status, GrpcTrailers) -> Unit)
+    public fun close(status: Status, trailers: GrpcTrailers = GrpcTrailers()): Nothing
     public fun proceed(request: Flow<Request>): Flow<Response>
+
+    public suspend fun FlowCollector<Response>.proceedFlow(request: Flow<Request>) {
+        proceed(request).collect {
+            emit(it)
+        }
+    }
 }
 
 public interface ServerInterceptor {
-    public fun <Request, Response> intercept(
-        scope: ServerCallScope<Request, Response>,
-        requestHeaders: GrpcTrailers,
+    public fun <Request, Response> ServerCallScope<Request, Response>.intercept(
         request: Flow<Request>,
     ): Flow<Response>
 }
