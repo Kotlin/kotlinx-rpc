3b36187a7b858fa22bb2efa17f78ddfb9a52e9f4
Johannes Zottele <official@johannes-zottele.at>
2025-09-17 10:13:30 +0200

    grpc: Refactor client scope API
    Signed-off-by: Johannes Zottele <official@johannes-zottele.at>

--- grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ClientInterceptor.kt
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/ClientInterceptor.kt
@@ -8,14 +8,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.rpc.grpc.internal.GrpcCallOptions
 import kotlinx.rpc.grpc.internal.MethodDescriptor
 
-/**
- * Represents a client call scope within a coroutine context, providing access to properties and
- * functions required to manage the lifecycle and behavior of a client-side remote procedure call
- * (RPC) in a coroutine-based environment.
- *
- * @param Request the type of the request message sent to the gRPC server.
- * @param Response the type of the response message received from the gRPC server.
- */
 public interface ClientCallScope<Request, Response> {
     public val method: MethodDescriptor<Request, Response>
     public val metadata: GrpcTrailers
@@ -32,13 +24,15 @@ public interface ClientInterceptor {
      * Intercepts and transforms the flow of requests and responses in a client call.
      * An interceptor can throw an exception at any time to cancel the call.
      *
-     * @param scope The scope of the client call, providing context and methods for managing
+     * The interceptor must ensure that it emits an expected number of values.
+     * E.g. if the intercepted method is a unary call, the interceptor's returned flow must emit exactly one value.
+     *
+     * @param this The scope of the client call, providing context and methods for managing
      * the call lifecycle and metadata.
      * @param request A flow of requests to be sent to the server.
      * @return A flow of responses received from the server.
      */
-    public fun <Request, Response> intercept(
-        scope: ClientCallScope<Request, Response>,
+    public fun <Request, Response> ClientCallScope<Request, Response>.intercept(
         request: Flow<Request>,
     ): Flow<Response>
 
