5cd7466ed6a36633b0e28e75151f579ab45d0b5e
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2025-10-14 15:05:15 +0200

    Update compiler tests infra (#510)

--- tests/compiler-plugin-tests/src/test/kotlin/kotlinx/rpc/codegen/test/runners/BaseTestRunner.kt
+++ tests/compiler-plugin-tests/src/test/kotlin/kotlinx/rpc/codegen/test/runners/BaseTestRunner.kt
@@ -7,30 +7,10 @@ package kotlinx.rpc.codegen.test.runners
 import kotlinx.rpc.codegen.test.services.ExtensionRegistrarConfigurator
 import kotlinx.rpc.codegen.test.services.RpcCompileClasspathProvider
 import kotlinx.rpc.codegen.test.services.RpcRuntimeClasspathProvider
-import org.jetbrains.kotlin.test.FirParser
 import org.jetbrains.kotlin.test.builders.TestConfigurationBuilder
 import org.jetbrains.kotlin.test.directives.CodegenTestDirectives
 import org.jetbrains.kotlin.test.directives.FirDiagnosticsDirectives
 import org.jetbrains.kotlin.test.directives.JvmEnvironmentConfigurationDirectives
-import org.jetbrains.kotlin.test.initIdeaConfiguration
-import org.jetbrains.kotlin.test.runners.AbstractFirPhasedDiagnosticTest
-import org.jetbrains.kotlin.test.services.EnvironmentBasedStandardLibrariesPathProvider
-import org.jetbrains.kotlin.test.services.KotlinStandardLibrariesPathProvider
-import org.junit.jupiter.api.BeforeAll
-
-abstract class BaseTestRunner : AbstractFirPhasedDiagnosticTest(FirParser.LightTree) {
-    companion object {
-        @BeforeAll
-        @JvmStatic
-        fun setUp() {
-            initIdeaConfiguration()
-        }
-    }
-
-    override fun createKotlinStandardLibrariesPathProvider(): KotlinStandardLibrariesPathProvider {
-        return EnvironmentBasedStandardLibrariesPathProvider
-    }
-}
 
 fun TestConfigurationBuilder.commonFirWithPluginFrontendConfiguration() {
     defaultDirectives {
