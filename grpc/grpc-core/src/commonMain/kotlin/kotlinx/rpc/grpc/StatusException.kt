e0e7dbffa2c11d8cc9065a153645f9576ab7129f
Johannes Zottele <official@johannes-zottele.at>
2025-11-24 12:29:04 +0100

    grpc: Add documentation

--- grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/StatusException.kt
+++ grpc/grpc-core/src/commonMain/kotlin/kotlinx/rpc/grpc/StatusException.kt
@@ -7,7 +7,20 @@ package kotlinx.rpc.grpc
 import kotlinx.rpc.internal.utils.InternalRpcApi
 
 /**
- * [Status] in Exception form, for propagating Status information via exceptions.
+ * Exception used for propagating gRPC status information in non-OK results.
+ *
+ * This is the primary mechanism for reporting and handling errors in gRPC calls.
+ * When a server encounters an error, it typically throws a [StatusException] with an appropriate
+ * [StatusCode] to signal the failure to the client. Clients receive this exception when
+ * remote calls fail with a non-OK status.
+ *
+ * The easiest way to construct a [StatusException] is to use the [StatusCode.asException] extension function:
+ * ```
+ * throw StatusCode.UNAUTHORIZED.asException("Authentication failed")
+ * ```
+ *
+ * @see Status
+ * @see StatusCode
  */
 public expect class StatusException : Exception {
     public constructor(status: Status)
@@ -17,7 +30,14 @@ public expect class StatusException : Exception {
     internal fun getTrailers(): GrpcMetadata?
 }
 
+/**
+ * The status associated with this exception.
+ */
 public val StatusException.status: Status get() = getStatus()
+
+/**
+ * The trailing metadata associated with this exception, or null if not present.
+ */
 public val StatusException.trailers: GrpcMetadata? get() = getTrailers()
 
 @InternalRpcApi
