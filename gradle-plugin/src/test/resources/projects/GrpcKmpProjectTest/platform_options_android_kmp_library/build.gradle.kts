53a07f350fe5259982fc011e421b5de87a80b6d5
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2026-02-12 12:22:07 +0100

    Added platform options for gradle plugin

---
+++ gradle-plugin/src/test/resources/projects/GrpcKmpProjectTest/platform_options_android_kmp_library/build.gradle.kts
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023-2025 JetBrains s.r.o and contributors. Use of this source code is governed by the Apache 2.0 license.
+ */
+
+import org.gradle.kotlin.dsl.version
+
+plugins {
+    kotlin("multiplatform") version "<kotlin-version>"
+    id("org.jetbrains.kotlinx.rpc.plugin") version "<rpc-version>"
+    id("com.android.kotlin.multiplatform.library") version "<android-version>"
+}
+
+kotlin {
+    jvm()
+    androidLibrary {
+        namespace = "com.example.namespace"
+        compileSdk = 34
+
+        withDeviceTestBuilder {
+            sourceSetTreeName = "test"
+        }
+
+        withHostTestBuilder {
+            sourceSetTreeName = "test"
+        }
+    }
+}
+
+rpc {
+    protoc()
+}
