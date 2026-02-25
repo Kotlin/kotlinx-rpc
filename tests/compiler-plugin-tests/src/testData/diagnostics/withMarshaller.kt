9af635ac6faf8839492ae13e88525612ba7969e6
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2026-02-25 15:14:00 +0100

    Update compiler tests

--- tests/compiler-plugin-tests/src/testData/diagnostics/withMarshaller.kt
+++ tests/compiler-plugin-tests/src/testData/diagnostics/withMarshaller.kt
@@ -8,99 +8,99 @@
 
 @file:OptIn(kotlinx.rpc.internal.utils.ExperimentalRpcApi::class)
 
-import kotlinx.rpc.grpc.codec.codec
-import kotlinx.rpc.grpc.codec.CodecConfig
-import kotlinx.rpc.grpc.codec.WithMarshaller
-import kotlinx.rpc.grpc.codec.MessageCodec
+import kotlinx.rpc.grpc.marshaller.marshallerOf
+import kotlinx.rpc.grpc.marshaller.MarshallerConfig
+import kotlinx.rpc.grpc.marshaller.WithMarshaller
+import kotlinx.rpc.grpc.marshaller.MessageMarshaller
 import kotlinx.io.Source
 
-@WithMarshaller(TestCodec::class)
+@WithMarshaller(TestMarshaller::class)
 open class Test
 
-@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestCodec::class<!>)
+@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestMarshaller::class<!>)
 class Test1
 
-@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestCodec::class<!>)
+@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestMarshaller::class<!>)
 class Test2 : Test()
 
-object TestCodec : MessageCodec<Test> {
-    override fun encode(value: Test, config: CodecConfig?): Source {
+object TestMarshaller : MessageMarshaller<Test> {
+    override fun encode(value: Test, config: MarshallerConfig?): Source {
         error("Not implemented")
     }
 
-    override fun decode(source: Source, config: CodecConfig?): Test {
+    override fun decode(source: Source, config: MarshallerConfig?): Test {
         error("Not implemented")
     }
 }
 
-@WithMarshaller(<!NOT_AN_OBJECT_REFERENCE_IN_WITH_MARSHALLER_ANNOTATION!>TestCodec3::class<!>)
+@WithMarshaller(<!NOT_AN_OBJECT_REFERENCE_IN_WITH_MARSHALLER_ANNOTATION!>TestMarshaller3::class<!>)
 class Test3
 
-class TestCodec3 : MessageCodec<Test3> {
-    override fun encode(value: Test3, config: CodecConfig?): Source {
+class TestMarshaller3 : MessageMarshaller<Test3> {
+    override fun encode(value: Test3, config: MarshallerConfig?): Source {
         error("Not implemented")
     }
 
-    override fun decode(source: Source, config: CodecConfig?): Test3 {
+    override fun decode(source: Source, config: MarshallerConfig?): Test3 {
         error("Not implemented")
     }
 }
 
-@WithMarshaller(TestCodec4::class)
+@WithMarshaller(TestMarshaller4::class)
 class Test4
 
-object TestCodec4 : ATestCodec4(), Whatever
+object TestMarshaller4 : ATestMarshaller4(), Whatever
 
 interface Whatever
 
-abstract class ATestCodec4 : MessageCodec<Test4> {
-    override fun encode(value: Test4, config: CodecConfig?): Source {
+abstract class ATestMarshaller4 : MessageMarshaller<Test4> {
+    override fun encode(value: Test4, config: MarshallerConfig?): Source {
         error("Not implemented")
     }
 
-    override fun decode(source: Source, config: CodecConfig?): Test4 {
+    override fun decode(source: Source, config: MarshallerConfig?): Test4 {
         error("Not implemented")
     }
 }
 
-@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestCodec4::class<!>)
+@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestMarshaller4::class<!>)
 class Test5
 
-interface CustomCodec1<A> : MessageCodec<A>
-interface CustomCodec2<B> : MessageCodec<B>
-interface CustomCodec3<C> : CustomCodec2<C>
-interface CustomCodec4<D> : CustomCodec3<D>, CustomCodec1<D>
+interface CustomMarshaller1<A> : MessageMarshaller<A>
+interface CustomMarshaller2<B> : MessageMarshaller<B>
+interface CustomMarshaller3<C> : CustomMarshaller2<C>
+interface CustomMarshaller4<D> : CustomMarshaller3<D>, CustomMarshaller1<D>
 
-@WithMarshaller(TestCodec8::class)
+@WithMarshaller(TestMarshaller8::class)
 class Test8
 
-object TestCodec8 : CustomCodec4<Test8> {
-    override fun encode(value: Test8, config: CodecConfig?): Source {
+object TestMarshaller8 : CustomMarshaller4<Test8> {
+    override fun encode(value: Test8, config: MarshallerConfig?): Source {
         error("Not implemented")
     }
 
-    override fun decode(source: Source, config: CodecConfig?): Test8 {
+    override fun decode(source: Source, config: MarshallerConfig?): Test8 {
         error("Not implemented")
     }
 }
 
-@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestCodec9::class<!>)
+@WithMarshaller(<!MARSHALLER_TYPE_MISMATCH!>TestMarshaller9::class<!>)
 class Test9
 
-object TestCodec9 : CustomCodec4<Test8> {
-    override fun encode(value: Test8, config: CodecConfig?): Source {
+object TestMarshaller9 : CustomMarshaller4<Test8> {
+    override fun encode(value: Test8, config: MarshallerConfig?): Source {
         error("Not implemented")
     }
 
-    override fun decode(source: Source, config: CodecConfig?): Test8 {
+    override fun decode(source: Source, config: MarshallerConfig?): Test8 {
         error("Not implemented")
     }
 }
 
 class Test10
-val testCodec10 = codec<<!CHECKED_ANNOTATION_VIOLATION!>Test10<!>>()
+val testMarshaller10 = marshallerOf<<!CHECKED_ANNOTATION_VIOLATION!>Test10<!>>()
 
-val testCodec = codec<Test>()
+val testMarshaller = marshallerOf<Test>()
 
 /* GENERATED_FIR_TAGS: annotationUseSiteTargetFile, classDeclaration, classReference, functionDeclaration,
 interfaceDeclaration, nullableType, objectDeclaration, override, stringLiteral, typeParameter */
