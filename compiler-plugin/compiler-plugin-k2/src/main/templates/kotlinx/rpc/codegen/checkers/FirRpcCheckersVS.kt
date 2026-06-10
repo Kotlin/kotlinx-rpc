feddc1b88907637e5546ecf1e6a8c22f56295ca8
Alexander Sysoev <Alexander.Sysoev@jetbrains.com>
2026-06-10 16:32:25 +0200

    Remove 2.1 Kotlin support and add 2.4 to docs (#724)

--- compiler-plugin/compiler-plugin-k2/src/main/templates/kotlinx/rpc/codegen/checkers/FirRpcCheckersVS.kt
+++ compiler-plugin/compiler-plugin-k2/src/main/templates/kotlinx/rpc/codegen/checkers/FirRpcCheckersVS.kt
@@ -22,170 +22,79 @@ import org.jetbrains.kotlin.fir.expressions.FirFunctionCall
 class FirCheckedAnnotationFunctionCallCheckerVS(
     private val ctx: FirCheckersContext,
 ) : FirFunctionCallChecker(MppCheckerKind.Common) {
-    //##csm FirCheckedAnnotationFunctionCallCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(expression: FirFunctionCall, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirCheckedAnnotationFunctionCallChecker.check(ctx, expression, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(expression: FirFunctionCall) {
         FirCheckedAnnotationFunctionCallChecker.check(ctx, expression, context, reporter)
     }
-    //##csm /default
-    //##csm /FirCheckedAnnotationFunctionCallCheckerVS_context
 }
 
 class FirCheckedAnnotationTypeParameterCheckerVS(
     private val ctx: FirCheckersContext,
 ) : FirTypeParameterChecker(MppCheckerKind.Common) {
-    //##csm FirCheckedAnnotationTypeParameterCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirTypeParameter, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirCheckedAnnotationTypeParameterChecker.check(ctx, declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirTypeParameter) {
         FirCheckedAnnotationTypeParameterChecker.check(ctx, declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirCheckedAnnotationTypeParameterCheckerVS_context
 }
 
 class FirCheckedAnnotationFirClassCheckerVS(
     private val ctx: FirCheckersContext,
 ) : FirClassChecker(MppCheckerKind.Common) {
-    //##csm FirCheckedAnnotationFirClassCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirCheckedAnnotationFirClassChecker.check(ctx, declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirClass) {
         FirCheckedAnnotationFirClassChecker.check(ctx, declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirCheckedAnnotationFirClassCheckerVS_context
 }
 
 class FirCheckedAnnotationFirFunctionCheckerVS(
     private val ctx: FirCheckersContext,
 ) : FirFunctionChecker(MppCheckerKind.Common) {
-    //##csm FirCheckedAnnotationFirFunctionCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirFunction, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirCheckedAnnotationFirFunctionChecker.check(ctx, declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirFunction) {
         FirCheckedAnnotationFirFunctionChecker.check(ctx, declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirCheckedAnnotationFirFunctionCheckerVS_context
 }
 
 class FirRpcAnnotationCheckerVS : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirRpcAnnotationCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirRpcAnnotationChecker.check(declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirRpcAnnotationChecker.check(declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirRpcAnnotationCheckerVS_context
 }
 
 class FirRpcServiceDeclarationCheckerVS(
     private val ctx: FirCheckersContext,
 ) : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirRpcServiceDeclarationCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirRpcServiceDeclarationChecker.check(ctx, declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirRpcServiceDeclarationChecker.check(ctx, declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirRpcServiceDeclarationCheckerVS_context
 }
 
 class FirRpcStrictModeClassCheckerVS : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirRpcStrictModeClassCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirRpcStrictModeClassChecker.check(declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirRpcStrictModeClassChecker.check(declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirRpcStrictModeClassCheckerVS_context
 }
 
 class FirGrpcServiceDeclarationCheckerVS : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirGrpcServiceDeclarationCheckerVS_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirGrpcServiceDeclarationChecker.check(declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirGrpcServiceDeclarationChecker.check(declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirGrpcServiceDeclarationCheckerVS_context
 }
 
 class FirWithGrpcMarshallerDeclarationCheckerVS : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirWithGrpcMarshallerDeclarationChecker_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirWithGrpcMarshallerDeclarationChecker.check(declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirWithGrpcMarshallerDeclarationChecker.check(declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirWithGrpcMarshallerDeclarationChecker_context
 }
 
 class FirProtoMessageAnnotationCheckerVS : FirRegularClassChecker(MppCheckerKind.Common) {
-    //##csm FirWithGrpcMarshallerDeclarationChecker_context
-    //##csm specific=[2.1.0...2.1.21, 2.2.0-ij251-*]
-    override fun check(declaration: FirRegularClass, context: CheckerContext, reporter: DiagnosticReporter) {
-        FirProtoMessageAnnotationChecker.check(declaration, context, reporter)
-    }
-    //##csm /specific
-    //##csm default
     context(context: CheckerContext, reporter: DiagnosticReporter)
     override fun check(declaration: FirRegularClass) {
         FirProtoMessageAnnotationChecker.check(declaration, context, reporter)
     }
-    //##csm /default
-    //##csm /FirWithGrpcMarshallerDeclarationChecker_context
 }
-
